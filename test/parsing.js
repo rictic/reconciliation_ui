7bf0d714469cb0762be52fc73473dd1193412d4b
Peter Burns <peter@metaweb.com>
2009-10-06 16:55:28 -0700

    Moved freebase mock code to test/mocking.js

--- test/parsing.js
+++ test/parsing.js
@@ -1,37 +1,3 @@
-var value_prop = {
-    "expected_type" : {
-        "extends" : ["/type/value"],
-        "id" : null,
-        "/freebase/type_hints/mediator" : {"optional":true, "value":null}
-    },
-    "reverse_property" : null,
-    "master_property"  : null,
-    "type" : "/type/property",
-    "name":null,
-    "id" : null
-}
-var topic_prop = {
-    "expected_type" : {
-        "extends" : [],
-        "id" : null,
-        "/freebase/type_hints/mediator" : {"optional":true, "value":null}
-    },
-    "reverse_property" : null,
-    "master_property"  : null,
-    "type" : "/type/property",
-    "name":null,
-    "id" : null
-}
-var freebase = {
-    fetchPropertyInfo:function(props,onComplete,onError){onComplete();},
-    getPropMetadata:function(prop){
-        if (prop.substr(0,8) == "/rec_ui/") return undefined;
-        if (prop.substr(0,5) == "topic") return topic_prop;
-        return value_prop;
-    },
-    fetchTypeInfo:function(types, onComplete, onError) {onComplete();}
-}
-
 var tests = {
     "test parsing a simple TSV": function() {
         var simpleTSV = "\
