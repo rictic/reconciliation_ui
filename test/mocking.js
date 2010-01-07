5b36696cb20da579707f9ad9ab381087578af0bf
Peter Burns <peter@metaweb.com>
2010-01-07 13:37:58 -0800

    Updated tests for new triple generation, uncommented CVT triple test

--- test/mocking.js
+++ test/mocking.js
@@ -27,6 +27,13 @@ var topic_prop = {
     "id" : null
 };
 
+var cvt_type = {
+    type:'/type/type',
+    "/freebase/type_hints/mediator" : {value:true},
+    "/freebase/type_hints/included_types":[]
+}
+
+
 var mocked_properties = {
   "/film/film/directed_by": {
         "expected_type": {
@@ -46,7 +53,21 @@ freebase.fetchPropertyInfo = function(props,onComplete,onError){onComplete();},
 freebase.getPropMetadata = function(prop){
     if (prop in mocked_properties && !(mocked_properties[prop] instanceof Function)) return mocked_properties[prop];
     if (startsWith("/rec_ui/", prop)) return undefined;
-    if (startsWith("topic", prop)) return topic_prop;
+    if (endsWith("topic", prop)) return topic_prop;
     return value_prop;
 }
 freebase.fetchTypeInfo = function(types, onComplete, onError) {onComplete();}
+
+freebase.getTypeMetadata = function(type) {
+    if (startsWith("/cvt", type)) {
+        var result = clone(cvt_type);
+        result.id = type;
+        return result;
+    }
+    return {
+        id:type,
+        type:'/type/type',
+        "/freebase/type_hints/included_types":[],
+        "/freebase/type_hints/mediator":{}
+    }
+}
