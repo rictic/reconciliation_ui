3a59aadbe550397f2592c3eca484e92b5502f192
Peter Burns <peter@metaweb.com>
2010-03-23 15:04:59 -0700

    Revert "Removed option to assert naked properties, couldn't find a good use case"
    This reverts commit e49959db7f187c35305a384f4dd4ee42a6845e99.

--- src/output.js
+++ src/output.js
@@ -108,7 +108,7 @@ function renderSpreadsheet(onComplete) {
 function prepareTriples() {
     $(".renderingTriples").show();
     $(".triplesRendered").hide();
-    getTriples(entities, function(triples) {
+    getTriples(entities, $("#assert_naked_properties")[0].checked, function(triples) {
         politeMap(triples,function(val){return JSON.stringify(val)},
             function(encodedTriples) {
                 var tripleString = encodedTriples.join("\n");
@@ -123,7 +123,7 @@ function prepareTriples() {
 }
 
 var tripleGetterYielder;
-function getTriples(entities, callback) {
+function getTriples(entities, assertNakedProperties, callback) {
     tripleGetterYielder = new Yielder();
     function hasValidID(entity) {
         var id = getID(entity);
@@ -202,7 +202,7 @@ function getTriples(entities, callback) {
         if (!subject || !hasValidID(subject) || subject.isCVT())
             return;
         
-        
+        /* Assert each type and all included types exactly once */
         var types = new Set();
         function addType(type) {
             types.add(type);
@@ -210,15 +210,17 @@ function getTriples(entities, callback) {
             if (metadata)
                 types.addAll(metadata["/freebase/type_hints/included_types"]);
         }
-        /* Assert each type and all included types exactly once */
         $.each($.makeArray(subject['/type/object/type']), function(_, type){addType(type)});
-        /* Assert any types implied by the subject's properties. */
-        $.each(subject['/rec_ui/headerPaths'], function(_, headerPath) {
-            var prop = headerPath.parts[0].prop;
-            var metadata = freebase.getPropMetadata(prop);
-            if (metadata && metadata.schema && metadata.schema.id && metadata.schema.id !== "/type/object")
-                addType(metadata.schema.id);
-        });
+        /* Unless given specific OK to assert naked properties, assert
+           any types implied by the subject's properties. */
+        if (!assertNakedProperties) {
+            $.each(subject['/rec_ui/headerPaths'], function(_, headerPath) {
+                var prop = headerPath.parts[0].prop;
+                var metadata = freebase.getPropMetadata(prop);
+                if (metadata && metadata.schema && metadata.schema.id && metadata.schema.id !== "/type/object")
+                    addType(metadata.schema.id);
+            });
+        }
         $.each(types.getAll(), function(_,type) {
             if (type)
                 triples.push({s:getID(subject), p:"/type/object/type",o:type});
@@ -457,6 +459,7 @@ $(document).ready(function () {
         $(".uploadToFreeQ").show();
     });
     
+    $("#assert_naked_properties").change(function() { prepareTriples(); });
     $("#mdo_data_source").suggest({type:"/dataworld/information_source",
                                flyout:true,type_strict:"should"})
                          .bind("fb-select", function(e, data) { 
