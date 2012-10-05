882efb7e6d6e93660d17562d65092d94cb076c48
Peter Burns <rictic@google.com>
2012-10-04 19:11:05 -0700

    simple gapi definitions

---
+++ lib/gapi.d.ts
@@ -0,0 +1,20 @@
+declare interface GapiClientRequestParams {
+  path: string;
+  params: Object;
+}
+
+declare interface GapiClientRequest {
+  execute(callBack:(Object)=>void):void;
+}
+
+declare interface GapiClient {
+  setApiKey(key:string);
+  request(p:GapiClientRequestParams):GapiClientRequest;
+  load(api:string, version:string, onSetup:()=>void);
+}
+
+declare module gapi {
+  export var client : GapiClient;
+    //export request();
+    //declare request(p:GapiClientRequest):GapiClientQuery;
+}
