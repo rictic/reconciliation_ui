971cdd56650f5b44695d40ecf81ffb3157021af9
Peter Burns <rictic@google.com>
2014-01-31 18:58:09 -0800

    clean up some of the excess datatables coloration

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ if (window.location.hostname.match(/plunder/)) {
     setTimeout(function() {
       $('button.continue').click();
       setTimeout(function() {
-        $('#tabs ul li a[href="#spreadsheetRender"]').click()
+        // $('#tabs ul li a[href="#spreadsheetRender"]').click()
       }, 100)
     }, 2000);
   }, 400);
